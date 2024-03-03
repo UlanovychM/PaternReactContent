6faf52e11cb62e2f494ca0989ef5b7a615718902
Mykhailo Ulanovych <97468374+UlanovychM@users.noreply.github.com>
2024-03-03 19:43:28 +0200

    update

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,14 +1,15 @@
 import css from './Filter.module.css';
 
 export default function Filter({ value, onFilter }) {
-  return (
-    <div>
-      <p className={css.label}>Search by name</p>
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => onFilter(e.target.value)}
-      />
-    </div>
-  );
+	return (
+		<div>
+			<p className={css.label}>Search by name</p>
+			{/* Контрольований елемент */}
+			<input
+				type='text'
+				value={value}
+				onChange={e => onFilter(e.target.value)}
+			/>
+		</div>
+	);
 }
